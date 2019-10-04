9eeaae0302cb0e664b6964c463dfffafe36d11a2
blithe-communicator-6847 <lemonliveshere@gmail.com>
2019-10-04 19:25:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,31 @@
-function basicTeenager(age) {
 
+var age ='';
+
+function basicTeenager(age) {
+ if (age >=13 && <=19)
+ return "You are a teenager";
 }
+
 
 function teenager(age) {
-
+  if (age >=13 && <=19)
+  return "You are a teenager";
+ }
+  return "You are not a teenager";
 }
+
 
 function ageChecker(age) {
-
+  if (age >=13 && <=19)
+  return "You are a teenager";
+}else if (ageChecker){
+  if (age <13)
+  return "You are a kid!";
+}else if (ageChecker) {
+  if (age >19)
+  return "You are a grownup "
 }
+
 
 function ternaryTeenager(age) {
 
